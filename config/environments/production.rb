5cddef2eabc26aafccaf5f9e68cd35b8b1236b45
Mateus Cardoso <mcgs@cin.ufpe.br>
2017-06-26 21:52:27 +0000

    trabbbbbbbbbbb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,8 +40,8 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-  config.action_cable.url = 'wss://ifarra.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://ifarra.herokuapp.com', 'https://ifarra.herokuapp.com' ]
+  config.web_socket_server_url = "wss://action-cable-example.herokuapp.com/cable" 
+  onfig.action_cable.allowed_request_origins = ['https://ifarra.herokuapp.com', 'https://ifarra.herokuapp.com']
   
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
